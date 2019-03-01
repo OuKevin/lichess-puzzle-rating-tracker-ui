5cef612c8d24ba90554d50c50dfaf041c811a578
Kevin Ou <therealkevinou@yahoo.com>
2019-02-28 22:36:59 -0600

    cleaned up unused files, added nodemon and body parser

--- server.js
+++ server.js
@@ -1,11 +1,19 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 const app = express();
 const port = process.env.PORT || 2323;
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
-// console.log that your server is up and running
-app.listen(port, () => console.log(`Listening on port ${port}`));
-
-// create a GET route
-app.get('/express_backend', (req, res) => {
-  res.send({ express: 'YOUR EXPRESS BACKEND IS CONNECTED TO REACT' });
+app.get('/api/hello', (req, res) => {
+  console.log('hello')
+  res.send({ express: 'Hello From Express' });
 });
+app.post('/api/world', (req, res) => {
+  console.log(req.body);
+  res.send(
+    `I received your POST request. This is what you sent me: ${req.body.post}`,
+  );
+});
+
+app.listen(port, () => console.log(`Listening on port ${port}`));
