3cbcdd9ac21e10ace1b71304d2731ba98cb55818
Kevin Ou <therealkevinou@yahoo.com>
2019-04-13 11:23:16 -0500

    updated chart to be more responsive

--- src/containers/HistoricalChartContainer.jsx
+++ src/containers/HistoricalChartContainer.jsx
@@ -1,12 +1,13 @@
 import React, { useEffect, useState } from 'react';
 import {
-  LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip, Legend
+  LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip, Legend, ResponsiveContainer
 } from 'recharts';
 import CustomTooltip from '../components/CustomTooltip';
 import axios from 'axios';
 import sortBy from 'lodash/sortBy';
 import take from 'lodash/take';
 import testData from '../testData';
+import './historicalChartContainer.css'
 
 export default () => {
   const [data, setData] = useState([]);
@@ -22,39 +23,35 @@ export default () => {
   }, [])
 
   return (
-    <>
+    <div className='chart-container'>
       <h1>Puzzle Rating for: Kevinou</h1>
-      <LineChart
-        data={data}
-        width={730}
-        height={250}
-        margin={{
-          top: 5,
-          right: 30,
-          left: 20,
-          bottom: 5,
-        }}
-      >
-        <CartesianGrid strokeDasharray="3 3" />
-        <XAxis
-          dataKey="creation_date"
-          tick={false}
-          tickLine={false}
-        />
-        <YAxis
-          domain={['dataMin - 25', 'dataMax + 25']}
-        />
-        <Tooltip
-          content={CustomTooltip}
-        />
-        <Legend />
-        <Line
-          dot={false}
-          type="monotone"
-          dataKey="rating"
-          stroke="#8884d8"
-        />
-      </LineChart>
-    </>
+      <ResponsiveContainer width="100%" height="80%">
+        <LineChart
+          data={data}
+          margin={{
+            top: 5,
+            right: 30,
+            left: 20,
+            bottom: 5,
+          }}
+        >
+          <CartesianGrid strokeDasharray="3 3" />
+          <XAxis
+            dataKey="creation_date"
+            tick={false}
+            tickLine={false}
+          />
+          <YAxis domain={['dataMin - 25', 'dataMax + 25']} />
+          <Tooltip content={CustomTooltip} />
+          <Legend />
+          <Line
+            dot={false}
+            type="monotone"
+            dataKey="rating"
+            stroke="#8884d8"
+          />
+        </LineChart>
+      </ResponsiveContainer>
+    </div>
   )
 }
