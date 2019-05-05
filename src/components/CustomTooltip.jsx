73bb10d865ee8b620545f8e4d28bc4dd72e90b3d
Kevin Ou <therealkevinou@yahoo.com>
2019-05-04 22:02:28 -0500

    adding default value for rating

--- src/components/CustomTooltip.jsx
+++ src/components/CustomTooltip.jsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import formatDate from '../utils/formatDate';
+import get from 'lodash/get';
 import './CustomToooltip.css'
 
 const CustomTooltip = ({ active, payload, label }) => {
   if (active) {
     return (
       <div className="custom-tooltip">
-        <p>{`Rating : ${payload[0].value}`}</p>
+        <p>{`Rating : ${get(payload, '0.value', 0)}`}</p>
         <p>{`Date : ${formatDate(label)}`}</p>
       </div>
     );
