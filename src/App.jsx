6bafcae3e3c0895ebc1a86cd53d799ec6ed38a32
KKOU <kevin.ou@bayer.com>
2020-11-22 00:29:33 -0600

    Adding in theming

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,15 @@
 import React from 'react';
 import HistoricalChartContainer from './containers/HistoricalChartContainer'
+import { ThemeProvider } from '@material-ui/core/styles';
+import theme from './theme';
 import './App.css';
 
 const App = () =>
+<ThemeProvider theme={theme}>
   <div className="App">
     <HistoricalChartContainer />
   </div>
+  </ThemeProvider>
+
 
 export default App;
---
+++ src/theme.js
@@ -0,0 +1,11 @@
+import { createMuiTheme } from '@material-ui/core/styles';
+
+const theme = createMuiTheme({
+  palette: {
+    primary: {
+      main: '#8884d8',
+    },
+  },
+});
+
+export default theme
